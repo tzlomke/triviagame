f19cc9075eb39a5364a7c2148bfae9ccaa777d7e
Chieftjz <taylor.zlomke@gmail.com>
2018-11-04 13:13:49 -0700

    completed game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,16 @@
-// Global Variables
+//Variables
 var right = 0;
 var wrong = 0;
 var unanswered = 0;
 
-var questions = [
-    {
+var questions = [{
         question: "Who killed John Lennon?",
         answers: {
             a: "Mark David Chapman",
             b: "Charles Manson",
             c: "Cristin Keleher",
         },
-        correctAnswer: "a"
+        correctAnswer: "a",
     },
     {
         question: "What year did the Beatles play Red Rocks?",
@@ -21,7 +20,7 @@ var questions = [
             c: "1964",
             d: "1961",
         },
-        correctAnswer: "c"
+        correctAnswer: "c",
     },
     {
         question: "What song did the Beatles <em>not</em> play on the Ed Sullivan Show?",
@@ -31,62 +30,95 @@ var questions = [
             c: "All My Loving",
             d: "Love Me Do",
         },
-        correctAnswer: "d"
+        correctAnswer: "d",
     },
 ]
 
-// Generate Quiz
-function generateQuiz() {
-
-    // Store Answers
-    var answers;
-
-    // Store Output to Append to DOM
-    var output = [];
-
-    for (i = 0; i < questions.length; i++) {
-        answers = [];
-        for (letter in questions[i].answers) {
-            // Create Radio Buttons
-            answers.push(
-                "<input type='radio' name='question'" + i + "value=" + [i] + ">"
-                + questions[i].answers[letter]
-                )
-        }
-
-        // Push Questions to Output
-        output.push(
-            "<div class='question'><h1>" + questions[i].question + "</h1></div>"
-            + "<div class='answers'>" + answers.join(" ") + "</div>"
-        )
-    }
-
-    // Append to DOM Element
-    $("#quiz").append(output.join("") + "<br>" + "<button type='button'>DONE</button>");
-}
-            
-
-// Quiz Start
+// Document Ready
 $(document).ready(function () {
+
     $("#quiz").hide();
     $("#stats").hide();
+
+    // Generate Quiz
+    function generateQuiz() {
+
+        // Store Answers
+        var answers;
+
+        // Store Output to Append to DOM
+        var output = [];
+
+        for (i = 0; i < questions.length; i++) {
+            answers = [];
+            for (letter in questions[i].answers) {
+
+                // Create Radio Buttons
+                answers.push(
+                    "<label>" +
+                    "<input type='radio' name=question" + i + " value=" + letter + ">" +
+                    questions[i].answers[letter] +
+                    "</label>"
+                )
+            }
+
+            // Push Questions and Possible Answers to Output
+            output.push(
+                "<div class='question'><h1>" + questions[i].question + "</h1></div>" +
+                "<div class='answers'>" + answers.join(" ") + "</div>"
+            )
+        }
+
+        // Append to DOM Element
+        $("#quiz").append(output.join("") + "<br>" + "<button type='button' id='done'>DONE</button>");
+    }
+
+    // Quiz Start
     $("#start").on("click", function () {
 
         // Generate Quiz
         generateQuiz()
 
-        // Displays Quiz on DOM
+        // Displays Quiz on DOM, Hides Start Button
         $("#quiz").show();
         $("#start").hide();
 
+        // Score Quiz
+        function scoreQuiz() {
+            for (i = 0; i < questions.length; i++) {
+
+                console.log(questions[i].correctAnswer);
+                console.log($("input[name=question" + i + "]:checked").val());
+
+                if ($("input[name=question" + i + "]").is(":checked")) {
+                    if ($("input[name=question" + i + "]:checked").val() === questions[i].correctAnswer) {
+                        right++;
+                    } else {
+                        wrong++;
+                    }
+                } else {
+                    unanswered++;
+                }                 
+            }
+
+            // Write Results to DOM
+            $("#right").text(right);
+            $("#wrong").text(wrong);
+            $("#unanswered").text(unanswered);
+        }
+
+        
+
         // Timer Start
         setTimeout(function () {
+            scoreQuiz();
             $("#quiz").hide();
             $("#stats").show();
         }, 120000);
 
         // Done Button
         $("#done").on("click", function () {
+            scoreQuiz();
             $("#quiz").hide();
             $("#stats").show();
         });
